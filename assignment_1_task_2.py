5de6d61b156ed21601c56d917c9d3b76b785501d
pv9034-DS <pv9034@gmail.com>
2025-09-12 09:51:11 +0530

    remove end=

--- assignment_1_task_2.py
+++ assignment_1_task_2.py
@@ -1,5 +1,4 @@
 firstname = input("Enter your first name: ")
 lastname = input("Enter your last name: ")
 
-print("Hello,",firstname,lastname,end="")
-print("! Welcome to the Python program.")
+print("Hello,",firstname,lastname,"! Welcome to the Python program.")
